d2080b0e89dab8ca092919913872a41ab05a5ca9
Gagah Pangeran <gagahpangeran@gmail.com>
2018-10-11 12:43:15 +0700

    make models

--- statusmu/models.py
+++ statusmu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Statusmu(models.Model):
+    name = models.CharField(max_length=30)
+    status = models.CharField(max_length=300)
+    date = models.DateTimeField(default=timezone.now)
