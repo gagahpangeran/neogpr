f4e52afc4fc2138b51a515c94f83d4c65042b191
Gagah Pangeran <gagahpangeran@gmail.com>
2018-10-11 12:29:38 +0700

    Add models create test

--- statusmu/tests.py
+++ statusmu/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import resolve, reverse
+from .models import *
+from django.utils import timezone
 
 
 # Create your tests here.
@@ -11,3 +13,8 @@ class StatusmuUnitTest(TestCase):
     def test_statusmu_page_contains_hello(self):
         response = self.client.get('/')
         self.assertContains(response, '<h1>Hello Apa Kabar?</h1>')
+
+    def test_statusmu_model_can_create_new_status(self):
+        Statusmu.objects.create(name='test', status='hehe')
+        count_status = Statusmu.objects.all().count()
+        self.assertEqual(count_status, 1)
