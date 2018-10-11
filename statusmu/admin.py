2a8d243528e065ee439c445824763fe0459086a4
Gagah Pangeran <gagahpangeran@gmail.com>
2018-10-11 13:41:49 +0700

    Add form and views

--- statusmu/admin.py
+++ statusmu/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Statusmu
 
 # Register your models here.
+admin.site.register(Statusmu)
--- statusmu/views.py
+++ statusmu/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Statusmu
 
 # Create your views here.
 
 
 def index(request):
+    if(request.method == 'POST'):
+        data = {}
+        post_data = dict(request.POST.items())
+        data['name'] = post_data['name']
+        data['status'] = post_data['status']
+        Statusmu(**data).save()
+
     return render(request, 'landing.html')
